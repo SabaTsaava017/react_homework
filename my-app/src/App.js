43322a071d9feb06ccdb5790a9e1b1f6bb74c07c
SabaTsaava017 <sabacaava7@gmail.com>
2023-09-26 23:23:28 +0400

    create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello react App</h1>
+      <button />
+    </div>
+  );
+}
+
+export default App;
+
+function button(){
+  return <button>click</button>
+
+}
